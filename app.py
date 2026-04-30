9b789ba1e4c31feb9a4d6defa80f33b2b8685f99
sawalda1 <sawaldamoise03@gmail.com>
2026-04-30 22:52:42 +0100

    Update app.py

--- app.py
+++ app.py
@@ -15,6 +15,6 @@ app.register_blueprint(collect_bp)
 app.register_blueprint(dashboard_bp)
 app.register_blueprint(analysis_bp)
 
-if name == "main":
+if__name__ == "__main__":
     port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port)
