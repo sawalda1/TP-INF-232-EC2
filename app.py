02c78d317d217a1830d5059ea0ce40b68a22850f
sawalda1 <sawaldamoise03@gmail.com>
2026-04-30 22:30:22 +0100

    Update app to use environment variable for port
    Use environment variable for port configuration.

--- app.py
+++ app.py
@@ -13,6 +13,7 @@ app.register_blueprint(collect_bp)
 app.register_blueprint(dashboard_bp)
 app.register_blueprint(analysis_bp)
 
-
+import os
 if name == "main":
-    app.run(host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
