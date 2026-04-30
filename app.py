82ce6973e3a96b5464b5c034ea682b00a6361039
sawalda1 <sawaldamoise03@gmail.com>
2026-04-30 22:33:01 +0100

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,8 @@ from routes.collect import collect_bp
 from routes.dashboard import dashboard_bp
 from routes.analysis import analysis_bp
 
+import os
+
 app = Flask(__name__)
 
 init_db()
@@ -13,7 +15,6 @@ app.register_blueprint(collect_bp)
 app.register_blueprint(dashboard_bp)
 app.register_blueprint(analysis_bp)
 
-import os
 if name == "main":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
